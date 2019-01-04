6e02ba9db67d9c00f407f717efd2c46f02245943
Diego Navarro <diegon@remobjects.com>
2019-01-04 18:57:00 +0100

    Added more encoding tests

--- Tests/EncodingTests.pas
+++ Tests/EncodingTests.pas
@@ -16,6 +16,38 @@ type
     begin
       Assert.AreEqual(TestApplication2.DoBinaryRead(), 3.141592653589793);
     end;
+
+    method BinaryPutVaruintTest;
+    begin
+      var lBuf := TestApplication2.DoBinaryPutUvarint(1);
+      Assert.AreEqual(lBuf.Length, 1);
+      Assert.AreEqual(lBuf[0], 1);
+
+      lBuf := TestApplication2.DoBinaryPutUvarint(127);
+      Assert.AreEqual(lBuf.Length, 1);
+      Assert.AreEqual(lBuf[0], $7f);
+
+      lBuf := TestApplication2.DoBinaryPutUvarint(256);
+      Assert.AreEqual(lBuf.Length, 2);
+      Assert.AreEqual(lBuf[0], $80);
+      Assert.AreEqual(lBuf[1], $02);
+    end;
+
+    method HexTest;
+    begin
+      Assert.AreEqual(TestApplication2.DoEncodingHexDecode(), 'Hello Gopher!');
+
+      var lBuf := TestApplication2.DoEncodingHexEncode();
+      Assert.AreEqual(lBuf.Length, 26);
+      Assert.AreEqual(lBuf[0], 52);
+      Assert.AreEqual(lBuf[1], 56);
+    end;
+
+    method JsonTest;
+    begin
+
+    end;
+
   end;
 
 end.
