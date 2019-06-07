66fafe53e4a23822e4d6bbe388790de397148dae
Diego Navarro <diegon@remobjects.com>
2019-06-07 18:04:39 +0200

    Adding an image and a couple of mime tests

---
+++ Tests/ImageTests.pas
@@ -0,0 +1,16 @@
+﻿namespace GoldLibrary.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit,
+  RemObjects.Elements.RTL;
+
+type
+  ImageTests = public class(Test)
+  public
+    method ImageDecoderTest;
+    begin
+      TestApplication2.DoImageDecodeTest();
+    end;
+  end;
+
+end.
---
+++ Tests/MimeTests.pas
@@ -0,0 +1,22 @@
+﻿namespace GoldLibrary.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit,
+  RemObjects.Elements.RTL;
+
+type
+  MimeTests = public class(Test)
+  public
+    method WordDecoderTest;
+    begin
+      TestApplication2.DoMimeWordDecodeTest();
+    end;
+
+    method NewReaderTest;
+    begin
+      TestApplication2.DoNewReaderTest();
+    end;
+
+  end;
+
+end.
