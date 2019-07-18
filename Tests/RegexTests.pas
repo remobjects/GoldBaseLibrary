475f9af66a763c5d349b60706dd779d8c31cd56d
Westermann <fwestermann@me.com>
2019-07-18 11:13:58 +0200

    Add First Tests for regexp package

---
+++ Tests/RegexTests.pas
@@ -0,0 +1,24 @@
+﻿namespace GoldLibrary.Tests.Shared;
+
+uses
+  RemObjects.Elements.EUnit,
+  RemObjects.Elements.RTL;
+
+type
+  RegeexTests = public class(Test)
+
+  public
+    method SimpleMatchTest;
+    begin
+      Assert.IsTrue(TestApplication2.DoSimpleMatchTest());
+    end;
+
+
+    method MustCompileTest;
+    begin
+      Assert.IsTrue(TestApplication2.DoMustCompileTest());
+    end;
+
+  end;
+
+end.
