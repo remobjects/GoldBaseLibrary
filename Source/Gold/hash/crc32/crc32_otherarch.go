3d8a7c7d9dae5b1fd5ef19e9bf0f20a0e2fa1410
marc hoffman <mh@remobjects.com>
2023-09-07 07:53:10 -0400

    Build some files for arm64 that were set not to.

--- Source/Gold/hash/crc32/crc32_otherarch.go
+++ Source/Gold/hash/crc32/crc32_otherarch.go
@@ -1,8 +1,9 @@
-// Copyright 2011 The Go Authors. All rights reserved.
+﻿// Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !amd64,!amd64p32,!s390x,!ppc64le,!arm64
+// +build !amd64,!amd64p32,!s390x,!ppc64le
+//##,!arm64
 
 package crc32
 
